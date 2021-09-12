5b31ad121ad5e825d02c596f2f404722b33ff555
kasun clement <kasun@ceytechsystemsolutions.com>
2021-09-13 02:20:29 +0530

    new improvemnts

--- resources/views/auth/CustomerRegister.blade.php
+++ resources/views/auth/CustomerRegister.blade.php
@@ -6,16 +6,16 @@
 @section('pageStyles')
 
 <!-- Select2 -->
-<link rel="stylesheet" href="/plugins/select2/css/select2.min.css">
-<link rel="stylesheet" href="/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+<link rel="stylesheet" href="./plugins/select2/css/select2.min.css">
+<link rel="stylesheet" href="./plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
 <!-- Bootstrap4 Duallistbox -->
-<link rel="stylesheet" href="/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
+<link rel="stylesheet" href="./plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
 <!-- Theme style -->
-<link rel="stylesheet" href="/dist/css/adminlte.min.css">
+<link rel="stylesheet" href="./dist/css/adminlte.min.css">
 <!-- Google Font: Source Sans Pro -->
-<link rel="stylesheet" href="/plugins/sweetalert2/sweetalert2.min.css">
+<link rel="stylesheet" href="./plugins/sweetalert2/sweetalert2.min.css">
 <!-- sweet alert -->
-<link rel="stylesheet" href="/plugins/custom-css/graduate_update.css">
+<link rel="stylesheet" href="./plugins/custom-css/graduate_update.css">
 @endsection
 @section('content')
 
@@ -132,27 +132,27 @@
             <!-- Page script -->
 
             <!-- Select2 -->
-            <script src="../../plugins/select2/js/select2.full.min.js"></script>
+            <script src="./plugins/select2/js/select2.full.min.js"></script>
             <!-- sweetalert -->
-            <script src="../../plugins/sweetalert2/sweetalert2.min.js"></script>
+            <script src="./plugins/sweetalert2/sweetalert2.min.js"></script>
             <!-- validation -->
-            <script src="../../plugins/jquery-validation/jquery.validate.min.js"></script>
+            <script src="./plugins/jquery-validation/jquery.validate.min.js"></script>
             <!-- Bootstrap4 Duallistbox -->
-            <script src="../../plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+            <script src="./plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
             <!-- InputMask -->
-            <script src="../../plugins/moment/moment.min.js"></script>
-            <script src="../../plugins/inputmask/min/jquery.inputmask.bundle.min.js"></script>
+            <script src="./plugins/moment/moment.min.js"></script>
+            <script src="./plugins/inputmask/min/jquery.inputmask.bundle.min.js"></script>
 
-            <script src="../../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+            <script src="./plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
             <!-- Bootstrap Switch -->
-            <script src="../../plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
-            <script src="../../dist/js/adminlte.min.js"></script>
+            <script src="./plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
+            <script src="./dist/js/adminlte.min.js"></script>
             <!-- AdminLTE for demo purposes -->
-            <script src="../../dist/js/demo.js"></script>
-            <script src="../../js/userjs/submit.js"></script>
-            <script src="../../js/registrationJs/registration.js"></script>
-            <script src="../../js/commenFunctions/file_upload.js"></script>
-            <script src="../../plugins/typeahead/typeahead.js"></script>
+            <script src="./dist/js/demo.js"></script>
+            <script src="./js/userjs/submit.js"></script>
+            <script src="./js/registrationJs/registration.js"></script>
+            <script src="./js/commenFunctions/file_upload.js"></script>
+            <script src="./plugins/typeahead/typeahead.js"></script>
 
             <!-- AdminLTE App -->
             <script>
