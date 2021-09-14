a903d524882a1944d3eb297f55e154085ad1b9df
kasun clement <kasun@ceytechsystemsolutions.com>
2021-09-15 01:54:23 +0530

    post profile created

--- resources/views/registration/post_registration.blade.php
+++ resources/views/registration/post_registration.blade.php
@@ -5,39 +5,39 @@
 @extends('layouts.footer')
 @section('pageStyles')
 <!-- Select2 -->
-<link rel="stylesheet" href="../plugins/select2/css/select2.min.css">
+<link rel="stylesheet" href="{{asset("/plugins/select2/css/select2.min.css")}}">
 
-<link rel="stylesheet" href="../plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+<link rel="stylesheet" href="{{asset("/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css")}}">
 <!-- Bootstrap4 Duallistbox -->
-<link rel="stylesheet" href="../plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
+<link rel="stylesheet" href="{{asset("/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css")}}">
 <!-- Theme style -->
 <!--<link rel="stylesheet" href="./dist/css/adminlte.min.css">-->
 <!-- Google Font: Source Sans Pro -->
-<link rel="stylesheet" href="../plugins/sweetalert2/sweetalert2.min.css">
+<link rel="stylesheet" href="{{asset("/plugins/sweetalert2/sweetalert2.min.css")}}">
 <!-- sweet alert -->
 <!-- Google Font: Source Sans Pro -->
 <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700&display=fallback">
 <!-- Font Awesome Icons -->
-<link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="{{asset("/plugins/fontawesome-free/css/all.min.css")}}">
 <!-- Theme style -->
-<link rel="stylesheet" href="../dist/css/adminlte.min.css">
-<link rel="stylesheet" href="../lugins/select2/css/select2.min.css">
-<link rel="stylesheet" href="../plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css">
+<link rel="stylesheet" href="{{asset("/dist/css/adminlte.min.css")}}">
+<link rel="stylesheet" href="{{asset("/lugins/select2/css/select2.min.css")}}">
+<link rel="stylesheet" href="{{asset("/plugins/select2-bootstrap4-theme/select2-bootstrap4.min.css")}}">
 <!-- Bootstrap4 Duallistbox -->
-<link rel="stylesheet" href="../plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css">
+<link rel="stylesheet" href="{{asset("/plugins/bootstrap4-duallistbox/bootstrap-duallistbox.min.css")}}">
 <!-- Theme style -->
-<link rel="stylesheet" href="../dist/css/adminlte.min.css">
+<link rel="stylesheet" href="{{asset("/dist/css/adminlte.min.css")}}">
 <!-- Google Font: Source Sans Pro -->
 <link href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,400i,700" rel="stylesheet">
 <!--tosts-->
 <!-- Toastr -->
-<link rel="stylesheet" href="../plugins/toastr/toastr.min.css">
+<link rel="stylesheet" href="{{asset("/plugins/toastr/toastr.min.css")}}">
 <!-- SweetAlert2 -->
-<link rel="stylesheet" href="../plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css">
-<link rel="stylesheet" href="../plugins/fontawesome-free/css/all.min.css">
+<link rel="stylesheet" href="{{asset("/plugins/sweetalert2-theme-bootstrap-4/bootstrap-4.min.css")}}">
+<link rel="stylesheet" href="{{asset("/plugins/fontawesome-free/css/all.min.css")}}">
 <!-- Ionicons -->
 <link rel="stylesheet" href="https://code.ionicframework.com/ionicons/2.0.1/css/ionicons.min.css">
-<link rel="stylesheet" href="../plugins/custom-css/graduate_update.css">
+<link rel="stylesheet" href="{{asset("/plugins/custom-css/graduate_update.css")}}">
 
 @endsection
 @section('content')
@@ -343,25 +343,25 @@
 <!-- Page script -->
 
 <!-- Select2 -->
-<script src="../plugins/select2/js/select2.full.min.js"></script>
+<script src="{{asset("/plugins/select2/js/select2.full.min.js")}}"></script>
 <!-- sweetalert -->
-<script src="../plugins/sweetalert2/sweetalert2.min.js"></script>
+<script src="{{asset("/plugins/sweetalert2/sweetalert2.min.js")}}"></script>
 <!-- validation -->
-<script src="../plugins/jquery-validation/jquery.validate.min.js"></script>
+<script src="{{asset("/plugins/jquery-validation/jquery.validate.min.js")}}"></script>
 <!-- Bootstrap4 Duallistbox -->
-<script src="../plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js"></script>
+<script src="{{asset("/plugins/bootstrap4-duallistbox/jquery.bootstrap-duallistbox.min.js")}}"></script>
 <!-- InputMask -->
-<script src="../plugins/moment/moment.min.js"></script>
-<script src="../plugins/inputmask/min/jquery.inputmask.bundle.min.js"></script>
+<script src="{{asset("/plugins/moment/moment.min.js")}}"></script>
+<script src="{{asset("/plugins/inputmask/min/jquery.inputmask.bundle.min.js")}}"></script>
 
-<script src="../plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js"></script>
+<script src="{{asset("/plugins/tempusdominus-bootstrap-4/js/tempusdominus-bootstrap-4.min.js")}}"></script>
 <!-- Bootstrap Switch -->
-<script src="../plugins/bootstrap-switch/js/bootstrap-switch.min.js"></script>
-<script src="../dist/js/adminlte.min.js"></script>
+<script src="{{asset("/plugins/bootstrap-switch/js/bootstrap-switch.min.js")}}"></script>
+<script src="{{asset("/dist/js/adminlte.min.js")}}"></script>
 <!-- AdminLTE for demo purposes -->
-<script src="../dist/js/demo.js"></script>
-<script src="../js/userjs/submit.js"></script>
-<script src="../js/commenFunctions/file_upload.js"></script>
+<script src="{{asset("/dist/js/demo.js")}}"></script>
+<script src="{{asset("/js/userjs/submit.js")}}"></script>
+<script src="{{asset("/js/commenFunctions/file_upload.js")}}"></script>
 
 <!-- AdminLTE App -->
 <script>
