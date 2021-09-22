6329aa403f13e4c98132e116c95379729bb99508
kasun clement <kasun@ceytechsystemsolutions.com>
2021-09-22 20:44:30 +0530

    updates adds, post bug fix

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -4,18 +4,6 @@
     <head>
         <meta name="csrf-token" content="{{--csrf_token()--}}" />
         <meta name="api-token" content="{{--auth()->user()->api_token--}}" />
-        <!-- Global site tag (gtag.js) - Google Analytics -->
-        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-208237465-1">
-        </script>
-        <script>
-            window.dataLayer = window.dataLayer || [];
-            function gtag() {
-                dataLayer.push(arguments);
-            }
-            gtag('js', new Date());
-
-            gtag('config', 'UA-208237465-1');
-        </script>
         @yield('styles')
     </head>
 
